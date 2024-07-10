31900e89fc54088b1080b20cdfe8bfed0c5cdc52
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-07-10 10:28:51 -0500

    practicando con el curso de basico de logica de programacion en alura

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.textContent = 'Juego del número secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.textContent = 'Indica un número del 1 al 10';
+
+function intentoDeUsuario(){
+    alert('click desde el boton');
+}
