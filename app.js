85dd93010bf7d605a6e4bb8c59680d851071e3e1
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-07-12 10:50:57 -0500

    add app.ts and index.html

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+let numeroSecreto = generarNumeroSecreto();
 
 
 function asignarTextoElemento(elemento, texto){
@@ -6,10 +6,20 @@ function asignarTextoElemento(elemento, texto){
     elementoHtml.innerHTML = texto;
 }
 
-function intentoDeUsuario(){
-    alert('click desde el boton');
+function verificarIntento(){
+    let numeroUsuario = parseInt(document.getElementById("valorInput").value);
+    console.log(numeroUsuario);
+    console.log(typeof(numeroUsuario));
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroSecreto === numeroUsuario);
+
 }
 
+
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10)+1;
+}
 
 asignarTextoElemento('h1', 'Juego del número secreto');
 asignarTextoElemento('p', 'Adivina un número del 1-10')
