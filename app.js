de0958fd1ea7c896cb106b89e5cf2b7073d30c44
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-07-19 22:06:32 -0500

    trabajando en el curso

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let numeroSecreto = generarNumeroSecreto();
-
+let intentos = 1;
+console.log(numeroSecreto);
 
 function asignarTextoElemento(elemento, texto){
     let elementoHtml = document.querySelector(elemento);
@@ -8,11 +9,23 @@ function asignarTextoElemento(elemento, texto){
 
 function verificarIntento(){
     let numeroUsuario = parseInt(document.getElementById("valorInput").value);
-    console.log(numeroUsuario);
-    console.log(typeof(numeroUsuario));
-    console.log(numeroSecreto);
-    console.log(typeof(numeroSecreto));
     console.log(numeroSecreto === numeroUsuario);
+    if(numeroSecreto === numeroUsuario){
+        asignarTextoElemento('p', `Genial has acertado en ${intentos} ${intentos===1 ? 'intento': 'intentos'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        
+    }else{
+
+        if(numeroSecreto > numeroUsuario){
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }else{
+            asignarTextoElemento('p', 'El número secreto es menor');    
+        }
+        intentos++;
+
+        limpiarCaja();
+
+    }
 
 }
 
@@ -22,4 +35,17 @@ function generarNumeroSecreto(){
 }
 
 asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Adivina un número del 1-10')
+asignarTextoElemento('p', 'Adivina un número del 1-10');
+
+function limpiarCaja(){
+    document.getElementById("valorInput").value='';
+}
+
+function nuevoJuego(){
+    intentos = 1;
+    numeroSecreto = generarNumeroSecreto();
+    console.log('Numero Secreto' + numeroSecreto);
+    asignarTextoElemento('p', 'Adivina un número del 1-10');
+    limpiarCaja();
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
+}
